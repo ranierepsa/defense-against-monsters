6030c10a7a573e6289b87fde1bbbae6e07b28094
Raniere Soares <ranierepsa@gmail.com>
2020-10-23 22:10:48 -0300

    Adding Splash Screen, Main Menu, Initial Level, 1 enemy animation and trophy animation

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int delayAfterSplashScreen = 3;
+    int currentLevel;
+
+    void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+        if (currentLevel == 0)
+            StartCoroutine(LoadStartScreenWithDelay(delayAfterSplashScreen));
+    }
+
+    IEnumerator LoadStartScreenWithDelay(int delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        SceneManager.LoadScene(currentLevel + 1);
+    }
+}
