d278cfa2e7b2ebc53b0d0714c16b04cf35d07475
Raniere Soares <ranierepsa@gmail.com>
2020-10-29 12:19:21 -0300

    Adding the Fox Attaker and doing some minor fixes
    Adding the Fox Attaker and doing some minor fixes

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)] float currentSpeed = 1f;
+    GameObject currentTarget;
 
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 
     public void SetCurrentSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        Defender defender = otherObject.GetComponent<Defender>();
+
+        if (defender)
+        {
+            if (otherObject.GetComponent<Gravestone>())
+            {
+                GetComponent<Animator>().SetTrigger("JumpTrigger");
+            } else
+            {
+                GetComponent<Attacker>().Attack(otherObject);
+            }
+        }
+    }
+}
